bc523952caaa856b0c461b02d8d015c8869a48a9
Edward <ed.tkleung@gmail.com>
2023-08-30 23:43:32 +0800

    ajax login

--- wall/public/js/index.js
+++ wall/public/js/index.js
@@ -25,6 +25,29 @@ login.addEventListener('submit', async (e) => {
   form.reset();
 })
 
+logout.addEventListener(onclick, async (e) => {
+  // e.preventDefault(); // Prevent the form from submitting synchronously
+  // Create your form object with the form inputs
+  // formObject["SomeCol"] = form.SomeCol.value;
+  const form = e.target
+  const formObj = {
+    username: form.username.value,
+    password: form.password.value
+  }
+  console.log(formObj);
+
+  const res = await fetch('/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(formObj)
+  });
+
+  // Clear the form here
+  form.reset();
+})
+
 memo.addEventListener('submit', async (e) => {
     e.preventDefault(); // Prevent the form from submitting synchronously
     // Create your form object with the form inputs
